7307c6cf965c3e71f990ceb2a1592b5185ecbc88
Guilherme Cortez <contato@mundogamer.com.br>
2015-05-25 14:39:57 -0300

    Atualização dos arquivos

--- server.js
+++ server.js
@@ -298,7 +298,7 @@ var MundoGamerApp = function() {
         self.app.post('/upload', admin.imageUpload);
 
         // send to facebook to do the authentication
-        self.app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email,public_profile,user_friends,user_location,user_birthday,user_activities, publish_stream' }));
+        self.app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email,public_profile,user_friends,user_location,user_birthday,user_activities' }));
         // handle the callback after facebook has authenticated the user
         self.app.get('/auth/facebook/callback',
                     passport.authenticate('facebook', {
